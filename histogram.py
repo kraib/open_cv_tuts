b9f61ce0292c23d72c733770edcf7d9fe8251094
kraiba <krabz01@gmail.com>
2016-08-02 10:26:50 +0300

    histogram with mask

--- histogram.py
+++ histogram.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy
+from matplotlib import pyplot as plt
 
-image=cv2.imread("images/blue.png",0)
-hist=cv2.calcHist([image],[0],None,[256],[0,256])
-print hist
+image=cv2.imread("images/blue.png")
+
+color=('b','g','r')
+for i,col in enumerate(color):
+    hist=cv2.calcHist([image],[i],None,[256],[0,256])
+    plt.plot(hist,color=col)
+    plt.xlim([0,256])
+plt.show()
