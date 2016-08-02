d41e0520b6254bc0db220832fa3bb86d4c8bc22b
kraiba <krabz01@gmail.com>
2016-08-02 10:49:56 +0300

    Remove axes from histogram

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -5,6 +5,6 @@ from matplotlib import pyplot as plt
 img = cv2.imread('images/wiki.png',0)
 equ = cv2.equalizeHist(img)
 
-plt.subplot(121),plt.imshow(img,cmap='gray'),plt.title('Input')
-plt.subplot(122),plt.imshow(equ,cmap='gray'),plt.title('Output')
+plt.subplot(121),plt.imshow(img,cmap='gray'),plt.title('Input'),plt.yticks([]),plt.xticks([])
+plt.subplot(122),plt.imshow(equ,cmap='gray'),plt.title('Output'),plt.yticks([]),plt.xticks([])
 plt.show()
