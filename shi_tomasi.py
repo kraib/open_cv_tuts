e0dba685027cc9f4686b762b8d12d0d337243150
kraiba <krabz01@gmail.com>
2016-08-09 09:12:39 +0300

    Shi and tomasi good features to track

---
+++ shi_tomasi.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('images/chess.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+corners= cv2.goodFeaturesToTrack(gray,14,0.01,5)
+print (corners)
+corners=np.int0(corners)
+print (corners)
+
+for i in corners:
+    print i
+    x,y=i.ravel()
+    cv2.circle(img,(x,y),3,255,1)
+plt.imshow(img),plt.show()
