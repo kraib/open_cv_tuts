e67bdfd17aca92a88e588e840282182c425883cc
kraiba <krabz01@gmail.com>
2016-08-10 09:24:12 +0300

    Add Feature macthing

--- shi_tomasi.py
+++ shi_tomasi.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('images/chess.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-corners= cv2.goodFeaturesToTrack(gray,14,0.01,5)
+corners= cv2.goodFeaturesToTrack(gray,30,0.01,5)
 print (corners)
 corners=np.int0(corners)
 print (corners)
@@ -12,5 +12,6 @@ print (corners)
 for i in corners:
     print i
     x,y=i.ravel()
-    cv2.circle(img,(x,y),3,255,1)
+    print i.ravel()
+    cv2.circle(img,(x,y),3,255,2)
 plt.imshow(img),plt.show()
