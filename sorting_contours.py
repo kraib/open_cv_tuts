5468cc920d3acd5477799f37bd1e648bc2ba648f
kraiba <krabz01@gmail.com>
2016-08-12 14:55:13 +0300

    plot sorted contours

--- sorting_contours.py
+++ sorting_contours.py
@@ -21,9 +21,9 @@ def draw_contour(image, c, i):
     M = cv2.moments(c)
     cX = 0
     cY = 0
-    if M['00'] != 00:
-        cX = int(M['m10'] / M['00'])
-        cY = int(M['m01'] / M['00'])
+    if M['m00'] != 0:
+        cX = int(M['m10'] / M['m00'])
+        cY = int(M['m01'] / M['m00'])
     cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
 
     return image
@@ -50,8 +50,10 @@ for (i,c) in enumerate(cnts):
     image=draw_contour(image,c,i)
 
 
-plt.subplot(2,2,1),plt.imshow(ord,cmap = 'gray'),plt.title('Original')
+plt.subplot(121,),plt.imshow(orig,cmap = 'gray'),plt.title('Original')
 plt.axis("off")
 
-plt.subplot(2,2,2),plt.imshow(image,cmap="gray"),plt.title('Sorted')
+plt.subplot(122),plt.imshow(image,cmap="gray"),plt.title('Sorted')
 plt.axis("off")
+
+plt.show()
