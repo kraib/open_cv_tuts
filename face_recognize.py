094aa36715658d69a3204a729f01081112696778
kraiba <krabz01@gmail.com>
2016-08-15 13:19:25 +0300

    chage to LBPH

--- face_recognize.py
+++ face_recognize.py
@@ -23,7 +23,7 @@ def read_images():
 [X,y]=read_images()
 y=np.asarray(y,dtype=np.int32)
 
-model=cv2.face.createEigenFaceRecognizer()
+model=cv2.face.createLBPHFaceRecognizer()
 model.train(np.asarray(X),np.asarray(y))
 camera =cv2.VideoCapture(0)
 face_cascade=cv2.CascadeClassifier("cascade/haarcascade_frontalface_default.xml")
@@ -34,11 +34,16 @@ while(True):
 
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         roi=gray[x:x+w,y:y+h]
-        roi = cv2.resize(roi, (200, 200),interpolation=cv2.INTER_LINEAR)
-        params = model.predict(roi)
-        print "Label: %s, Confidence: %.2f" % (params[0], params[1])
-        cv2.putText(img, "Kraiba Found", (x, y - 20),cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
-        cv2.rectangle(img,(x,y),((x+w),y+h),(255,255,0),1)
+        try:
+            roi = cv2.resize(roi, (200, 200),interpolation=cv2.INTER_LINEAR)
+            params = model.predict(roi)
+            if params[1] < 50:
+                print "Label: %s, Confidence: %.2f" % (params[0], params[1])
+
+                cv2.putText(img, "Kraiba Found", (x, y - 20),cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+                cv2.rectangle(img,(x,y),((x+w),y+h),(255,255,0),1)
+        except:
+            continue
     cv2.imshow("camera", img)
     if cv2.waitKey(1000 / 12) & 0xff == ord("q"):
         break
